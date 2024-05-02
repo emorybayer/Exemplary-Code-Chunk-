0863cbdb16187830664fb9a428650c36097866c7
emorybayer <156925745+emorybayer@users.noreply.github.com>
2024-05-02 15:21:39 -0400

    Add files via upload

---
+++ Annotated Exemplary Code Chunk.R
@@ -0,0 +1,45 @@
+
+#Loading, renaming, and summarizing data to create a table
+load("ssa_water.Rdata")
+New_set <- water %>%
+  group_by(countryModule) %>%
+  summarize(
+    N = n(),
+    Year = first(interviewYear),
+    Walk_time_Mean = mean(waterTimeMins, na.rm = TRUE),
+    SD_Walk_time = sd(waterTimeMins, na.rm = TRUE),
+    Median_Walk_time = median(waterTimeMins, na.rm = TRUE),
+    Percent_on_plot = (sum(waterTimeMins == 0, na.rm = TRUE) / N * 100)
+  )
+
+#Finding totals 
+sum(New_set$N)
+mean(New_set$Walk_time_Mean)
+mean(New_set$SD_Walk_time)
+mean(New_set$Median_Walk_time)
+mean(New_set$Percent_on_plot)
+
+#Creating total row to add to bottom of the table
+new_row = data.frame(
+  countryModule = c("Total"),
+  N = 623926,
+  Year = NA,
+  Walk_time_Mean = 22.92044,
+  SD_Walk_time = 32.25327,
+  Median_Walk_time = 12.82143,
+  Percent_on_plot = 23.78786
+)
+
+#Combining the total row with the table 
+New_new = rbind(New_set, new_row) %>%
+  rename( country = countryModule,
+          year = Year,
+          mean = Walk_time_Mean,
+          SD = SD_Walk_time,
+          med = Median_Walk_time,
+          `% on plot`= Percent_on_plot) 
+
+#Formatting the header for the data
+
+kable(New_new) 
+#%>% add_header_above(header = c(" " = 3, "walk time (min)" = 3, " " = 1))
